f836d037a90df2d03dc115a70d3ca30a498f8e89
Russell Stewart <russell@zmosoft.com>
2017-02-08 14:28:52 -0700

    Adding "CreateUser" cloud function.

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,27 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('CreateUser', function(req, res){
+    var callingUserId = req.params.userId;
+    var username = req.params.username;
+    var password = req.params.password;
+    var role = req.params.role;
+    
+    var queryUser = new Parse.Query(Parse.User);
+    queryUser.equalTo('username', username);
+    queryUser.first().then(function(user){
+        if (user == undefined) {
+            user = new Parse.User;
+            user.set('username', username);
+            user.set('password', password);
+            user.set('role', role);
+            user.signUp().then(function(user){
+                res.success('User Created');
+            });
+        }
+        else {
+            res.error('User Exists');
+        }
+    });
+});
