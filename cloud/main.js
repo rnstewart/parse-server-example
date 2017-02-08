71515a698cf798101509b7432b347b8663adf498
Russell Stewart <russell@zmosoft.com>
2017-02-08 15:38:58 -0700

    Removing logging from CreateUser cloud function.

--- cloud/main.js
+++ cloud/main.js
@@ -10,17 +10,10 @@ Parse.Cloud.define('CreateUser', function(req, res){
     var password = req.params.password;
     var role = req.params.role;
     
-    console.log('callingUserId = ' + callingUserId);
-    console.log('username = ' + username);
-    console.log('password = ' + password);
-    console.log('role = ' + role);
-    
     var queryUser = new Parse.Query(Parse.User);
     queryUser.equalTo('username', username);
-    console.log('queryUser = ' + JSON.stringify(queryUser));
     queryUser.first({
         success: function(user){
-            console.log('user = ' + JSON.stringify(user));
             if (user == undefined) {
                 user = new Parse.User();
                 user.set('username', username);
