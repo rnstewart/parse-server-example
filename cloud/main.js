8ccc8fb41df191426ea3056bf563d80411b3379c
Russell Stewart <russell@zmosoft.com>
2017-02-08 14:33:19 -0700

    Fixed a bug in CreateUser cloud function.

--- cloud/main.js
+++ cloud/main.js
@@ -11,13 +11,13 @@ Parse.Cloud.define('CreateUser', function(req, res){
     
     var queryUser = new Parse.Query(Parse.User);
     queryUser.equalTo('username', username);
-    queryUser.first().then(function(user){
+    return queryUser.first().then(function(user){
         if (user == undefined) {
             user = new Parse.User;
             user.set('username', username);
             user.set('password', password);
             user.set('role', role);
-            user.signUp().then(function(user){
+            return user.signUp().then(function(user){
                 res.success('User Created');
             });
         }
