db49cd7f59a4ec1fb5f85051fde6040e24cc58de
Russell Stewart <russell@zmosoft.com>
2017-02-08 16:03:10 -0700

    Added UpdateUser cloud function.

--- cloud/main.js
+++ cloud/main.js
@@ -66,20 +66,74 @@ Parse.Cloud.define('DeleteUser', function(req, res){
                 if (role == 3) {
                     var queryUser = new Parse.Query(Parse.User);
                     queryUser.equalTo('username', username);
-                    console.log('queryUser = ' + JSON.stringify(queryUser));
                     queryUser.first({
                         success: function(user){
-                            console.log('user = ' + JSON.stringify(user));
                             if (user != undefined) {
-                                console.log('destroy user.');
                                 user.destroy({
                                     useMasterKey: true,
                                     success: function(object){
-                                        console.log('User destroyed.');
                                         res.success('User Deleted.');
                                     },
                                     error: function(object, error){
-                                        console.log('ERORR: ' + error);
+                                        res.error(error);
+                                    }
+                                });
+                            }
+                            else {
+                                res.error('User Does Not Exist.');
+                            }
+                        },
+                        error: function(error){
+                            res.error(error);
+                        }
+                        
+                    });
+                }
+                else {
+                    res.error('User not authorized to do this.');
+                }
+            }
+            else {
+                res.error('User not authorized to do this.');
+            }
+        },
+        error: function(error){
+            res.error(error);
+        }
+    });
+});
+
+Parse.Cloud.define('UpdateUser', function(req, res){
+    var callingUserId = req.params.callingUserId;
+    var userId = req.params.userId;
+    var username = req.params.username;
+    var password = req.params.password;
+    var role = req.params.role;
+    
+    var queryUser = new Parse.Query(Parse.User);
+    queryUser.equalTo('objectId', callingUserId);
+    queryUser.first({
+        success: function(callingUser){
+            if (callingUser != undefined) {
+                var role = callingUser.get('role');
+                if (role == 3) {
+                    var queryUser = new Parse.Query(Parse.User);
+                    queryUser.equalTo('objectId', userId);
+                    console.log('queryUser = ' + JSON.stringify(queryUser));
+                    queryUser.first({
+                        success: function(user){
+                            if (user != undefined) {
+                                user.set('username', username);
+                                user.set('password', password);
+                                user.set('role', role);
+                                console.log('user = ' + JSON.stringify(user));
+                                user.save({
+                                    useMasterKey: true,
+                                    success: function(object){
+                                        console.log('User Updated.');
+                                        res.success('User Updated.');
+                                    },
+                                    error: function(object, error){
                                         res.error(error);
                                     }
                                 });
